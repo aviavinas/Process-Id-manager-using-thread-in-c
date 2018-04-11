47d0c155e781852f479269a0977ecb05fdb32f4b
aviavinas <32093890+aviavinas@users.noreply.github.com>
2018-04-12 00:01:53 +0530

    Update PID-Manager.c

--- PID-Manager.c
+++ PID-Manager.c
@@ -18,7 +18,7 @@ int allocate_map() {
 }
 int allocate_pid() {
 	for(int i = 0; i < SIZE; i++) {
-		if(bit_map[i] == 0) {
+		if(bit_map[i] == 0) { // just make sure that pid is available
 			bit_map[i] = 1; // Allocate a Pid to new process
       printf("New Process Allocated with PID : %d \n",(i+MIN_PID));
 			return i + MIN_PID;
